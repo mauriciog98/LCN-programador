9dd6b8c1e8fba7e0c0605a12b5916c5d572ea77c
Andres Mauricio Gallego <mauriciog98@yahoo.es>
2022-04-16 20:14:29 -0500

    logical structure to StudentController defined

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StudentFormRequest;
 use App\Models\Student;
 use Illuminate\Http\Request;
 
@@ -10,76 +11,61 @@ class StudentController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return array
      */
     public function index()
     {
-        //
+        $students = Student::all()->toArray();
+        return array_reverse($students);
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @param  \App\Http\Requests\StudentFormRequest  $request
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function create()
+    public function store(StudentFormRequest $request)
     {
-        //
-    }
+        $post = new Student($request->validated());
+        $post->save();
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+        return response()->json(__('The Student successfully added'));
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\Student  $student
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function show(Student $student)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Student  $student
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Student $student)
-    {
-        //
+        return response()->json($student);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Http\Requests\StudentFormRequest  $request
      * @param  \App\Models\Student  $student
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function update(Request $request, Student $student)
+    public function update(StudentFormRequest $request, Student $student)
     {
-        //
+        $student->update($request->validated());
+        return response()->json(__('The Student successfully updated'));
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Student  $student
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy(Student $student)
     {
-        //
+        $student->delete();
+        return response()->json(__('The Student successfully updated'));
     }
 }
