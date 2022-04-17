40d14c0c47d7f7a3eff2639c6c25c5d2361b7351
Andres Mauricio Gallego <mauriciog98@yahoo.es>
2022-04-16 19:57:41 -0500

    define fields on students database table

--- database/migrations/2022_04_17_003532_create_students_table.php
+++ database/migrations/2022_04_17_003532_create_students_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name',50);
+            $table->string('lastname',50);
+            $table->string('document_type',50);
+            $table->string('document_number',50);
+            $table->unsignedInteger('gender')->comment('0 undefined - 1 female - 2 male');
+            $table->date('birthdate');
+            $table->string('career',50);
             $table->timestamps();
         });
     }
